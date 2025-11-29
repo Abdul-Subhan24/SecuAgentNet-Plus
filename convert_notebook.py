931b95846a8250f50ae68e9b60730bf9a59800d9
Student Apn College <mdabdulsubhan0987@gmail.com>
2025-11-30 00:18:39 +0530

    Cleanup: remove runtime files, pycache, logs, temporary scripts

--- convert_notebook.py
+++
@@ -1,8 +0,0 @@
-import nbformat
-
-p = "notebook.ipynb"
-nb = nbformat.read(p, as_version=4)
-nb['cells'][0]['cell_type'] = 'markdown'
-nbformat.write(nb, p)
-
-print("Converted first cell to markdown. Now open notebook.ipynb → Run All → Save.")
